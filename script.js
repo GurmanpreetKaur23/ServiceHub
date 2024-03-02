84ed722d3c8f1a6eb2ac332bbdbb679937ee1cad
GurmanpreetKaur23 <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-03-02 16:21:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    // console.log(pos);
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+    let ScrollValue = Math.round((pos * 100) / calcHeight);
+    // console.log(ScrollValue);
+
+    if (pos > 100) {
+        scrollProgress.style.display = "grid";
+    } else {
+        scrollProgress.style.display = "none";
+    }
+
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+
+    scrollProgress.style.background = `conic-gradient(#F2E9FD ${ScrollValue}%, #d7d7d7 ${ScrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
