fd07f9ce9121e415676f97dc9ce298282f5d9115
GurmanpreetKaur23 <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-03-11 23:07:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,35 +27,4 @@ window.onscroll = calcScrollValue;
 window.onload = calcScrollValue;
 
 
-// connect
-// gurman
-
-let calcConnectValue = () => {
-    let connectProgress = document.getElementById("connect");
-    let posi = document.documentElement.scrollTop;
-    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let QuestionValue = Math.round((posi * 100) / calcHeight);
-
-    if (posi > 100) {
-        connectProgress.style.display = "grid";
-    } else {
-        connectProgress.style.display = "none";
-    }
-
-    connectProgress.addEventListener("click", () => {
-        document.documentElement.scrollTop = 0;
-    });
-
-    connectProgress.style.background = `conic-gradient(#f9aea9 ${QuestionValue}%, #d7d7d7 ${QuestionValue}%)`;
-};
-
-window.onscroll = () => {
-    calcScrollValue();
-    calcConnectValue(); // Call the function for handling the Connect button
-};
-
-window.onload = () => {
-    calcScrollValue();
-    calcConnectValue(); // Call the function for handling the Connect button
-};
 
